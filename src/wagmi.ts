fb1084bf501bd845451818db2ddf49e3dadadc02
aven <aven.tong@mail.fameex.info>
2025-10-01 20:52:46 +0400

    feat: add wallet

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -2,11 +2,12 @@ import { createConfig, http } from "wagmi";
 import { mainnet, sepolia } from "wagmi/chains";
 import { injected, walletConnect } from "wagmi/connectors";
 
-const walletConnectProjectId = (import.meta as any)?.env?.VITE_WALLETCONNECT_PROJECT_ID as string | undefined;
+// const walletConnectProjectId = (import.meta as any)?.env?.VITE_WALLETCONNECT_PROJECT_ID as string | undefined;
+// if (!walletConnectProjectId) {
+//   throw new Error("Missing WalletConnect projectId. Please set WALLETCONNECT_PROJECT_ID in your environment.");
+// }
 
-if (!walletConnectProjectId) {
-  throw new Error("Missing WalletConnect projectId. Please set WALLETCONNECT_PROJECT_ID in your environment.");
-}
+const projectId = "02b8455458e3a7e942090b71e18af269";
 
 export const config = createConfig({
   chains: [mainnet, sepolia],
@@ -14,7 +15,7 @@ export const config = createConfig({
     [mainnet.id]: http(),
     [sepolia.id]: http(),
   },
-  connectors: [injected(), walletConnect({ projectId: walletConnectProjectId })],
+  connectors: [injected(), walletConnect({ projectId })],
 });
 
 declare module "wagmi" {
